ff64df7107474c2457dcd144783f4d09c98c2389
Filip Pytloun <filip@pytloun.cz>
2015-08-21 21:07:56 +0200

    Support for footer text and link

--- postfix/files/admin/config.local.php
+++ postfix/files/admin/config.local.php
@@ -6,6 +6,7 @@ $CONF['postfix_admin_url'] = '{{ server.admin.url }}';
 $CONF['admin_email'] = '{{ server.admin.email }}';
 
 $CONF['encrypt'] = '{{ server.admin.encrypt }}';
+$CONF['vacation'] = '{% if server.admin.vacation.enabled %}YES{% else %}NO{% endif %}';
 $CONF['vacation_domain'] = '{{ server.admin.vacation.domain }}';
 
 $CONF['domain_path'] = 'YES';
@@ -13,7 +14,10 @@ $CONF['backup'] = 'NO';
 $CONF['used_quotas'] = 'YES';
 $CONF['new_quota_table'] = 'YES';
 
-$CONF['show_footer_text'] = 'NO';
+$CONF['show_footer_text'] = '{% if server.admin.footer.show_text %}YES{% else %}NO{% endif %}';
+$CONF['user_footer_link'] = "{{ server.admin.footer.link }}";
+$CONF['footer_text'] = '{{ server.admin.footer.text }}';
+$CONF['footer_link'] = '{{ server.admin.footer.link }}';
 
 {%- if server.admin.default_aliases %}
 $CONF['default_aliases'] = array (
